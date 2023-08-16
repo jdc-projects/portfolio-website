c46e3c22696aafd496ae61048ed1e13b41b358b6
Jack Chapman <jack.chapman@sky.com>
2023-08-16 20:57:05 +0100

    An import was added before ready.

--- frontend/mdx-components.tsx
+++ frontend/mdx-components.tsx
@@ -1,6 +1,5 @@
 import type { MDXComponents } from 'mdx/types'
 import { Title, Text, Anchor, Blockquote, Code } from '@mantine/core'
-import SyntaxHighlighter from 'react-syntax-highlighter'
 
 // This file allows you to provide custom React components
 // to be used in MDX files. You can import and use any

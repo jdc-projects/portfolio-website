4deb8ff471bb8aea5e0e4368ba3bb84fec19f481
Jack Chapman <jack.chapman@sky.com>
2023-08-23 23:58:46 +0100

    Improvements to make local public images work with wrapper component.

--- frontend/components/Image.tsx
+++ frontend/components/Image.tsx
@@ -1,10 +1,39 @@
 import NextImage from 'next/image'
 import { Image as MantineImage, ImageProps as MantineImageProps } from '@mantine/core'
+import sharp from 'sharp'
 
 type ImageProps = MantineImageProps & {
   alt: string,
 }
 
-export default function Image(props: ImageProps) {
-  return <MantineImage component={NextImage} {...props} height={1000} width={1000} />
+type Dimensions = {
+  width: number | undefined,
+  height: number | undefined,
+}
+
+export default async function Image(props: ImageProps) {
+  const dimensions : Dimensions = (props.src.src === undefined) ? await getImageDimensions(props.src) : {width: props.src.width, height: props.src.height}
+
+  return <MantineImage component={NextImage} {...props} width={dimensions.width} height={dimensions.height} />
+}
+
+async function getImageDimensions(src: string) {
+  let dimensions : Dimensions = {
+    width: undefined,
+    height: undefined,
+  }
+
+  const isLocalPublicPath = (src.at(0) === '/')
+
+  if (isLocalPublicPath) {
+    const filepath = 'public' + src
+    const metadata = await sharp(filepath).metadata()
+
+    dimensions = {
+      width: metadata.width,
+      height: metadata.height,
+    }
+  }
+
+  return dimensions
 }

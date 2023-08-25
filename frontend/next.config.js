682327f9ab6212d442068bf2e29c3c0a573e95c6
Jack Chapman <jack.chapman@sky.com>
2023-08-25 06:26:19 +0100

    Remove preact, since it wasn't doing much and may cause issues in the future.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -12,16 +12,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      Object.assign(config.resolve.alias, {
-        'react': 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      })
-    }
-    return config
-  },
 }
 
 const withMDX = require('@next/mdx')()

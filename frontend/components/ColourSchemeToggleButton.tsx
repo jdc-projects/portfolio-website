85453edc276be3d4758c446f2f5298c2c2f3cdcd
Jack Chapman <jack.chapman@sky.com>
2023-08-30 09:48:12 +0100

    Set the icon size and stroke for light/dark mode.

--- frontend/components/ColourSchemeToggleButton.tsx
+++ frontend/components/ColourSchemeToggleButton.tsx
@@ -8,11 +8,13 @@ type ColourSchemeToggleButtonProps = UnstyledButtonProps & {
 
 export default function ColourSchemeToggleButton(props: ColourSchemeToggleButtonProps) {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const iconSize = 32
+  const iconStroke = 1.5
 
   return (
     <UnstyledButton h={50} w={50} mr={5} {...props} onClick={() => toggleColorScheme()} >
       <Center>
-        {colorScheme === 'dark' ? <IconSun/> : <IconMoon/>}
+        {colorScheme === 'dark' ? <IconSun size={iconSize} stroke={iconStroke} /> : <IconMoon size={iconSize} stroke={iconStroke} />}
       </Center>
     </UnstyledButton>
   )

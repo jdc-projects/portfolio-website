b9fb00c504237b0895a9c78f72ed13041f27a8e9
Jack Chapman <jack.chapman@sky.com>
2023-08-30 05:25:03 +0100

    Reduce stroke size on social links.

--- frontend/components/SocialLinks.tsx
+++ frontend/components/SocialLinks.tsx
@@ -5,8 +5,8 @@ import Anchor from "components/Anchor";
 export default function SocialLinks() {
   return (
     <Flex direction='row' gap='xl' >
-      <Anchor href='https://github.com/jd-chapman' c='currentColor' ><IconBrandGithub size={48} /></Anchor>
-      <Anchor href='https://www.linkedin.com/in/jack-chapman-' c='currentColor' ><IconBrandLinkedin size={48} /></Anchor>
+      <Anchor href='https://github.com/jd-chapman' c='currentColor' ><IconBrandGithub size={48} stroke={1.5} /></Anchor>
+      <Anchor href='https://www.linkedin.com/in/jack-chapman-' c='currentColor' ><IconBrandLinkedin size={48} stroke={1.5} /></Anchor>
     </Flex>
   )
 }

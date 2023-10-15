b1f72a4c38c5af1a7b82e0f82d17990a29100e04
Jack Chapman <jack.chapman@sky.com>
2023-10-16 00:11:16 +0100

    Some simplification to mobile navigation component.

--- next-app/components/MobileNavigation.tsx
+++ next-app/components/MobileNavigation.tsx
@@ -14,30 +14,12 @@ type MobileNavigationProps = {
 
 export default function MobileNavigation(props: MobileNavigationProps) {
   const [opened, { toggle }] = useDisclosure()
-  const mobile = isMobile()
-
-  let navigation = <></>
-  if (mobile && opened) {
-    navigation = (
-      <GridCol span={12}>
-        <Flex gap='xl' justify='center' align='center' direction='column' h='100%' >
-          {props.navs.map((nav: nav) => {
-            return (
-              <Anchor href={nav.route} underline='never' c='currentColor' size='xl' fw={500} key={nav.name} >
-                {nav.name}
-              </Anchor>
-            )
-          })}
-        </Flex>
-      </GridCol>
-    )
-  }
 
   return (
     <>
       <GridCol span={2}>
         <Flex hiddenFrom='sm' justify='center' align='center' direction='row' h='100%' mr={50} >
-          {mobile ? <Burger opened={opened} onClick={toggle} aria-label="Toggle navigation" /> : null}
+          <Burger opened={opened} onClick={toggle} aria-label="Toggle navigation" />
         </Flex>
       </GridCol>
       <GridCol hiddenFrom='sm' span={12} m={0} p={0} >

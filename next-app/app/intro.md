7a181c42d21ab46875151884c732a79f6cbdd125
Jack Chapman <jack.chapman@sky.com>
2023-10-16 00:18:16 +0100

    Some more mobile fixing.

--- next-app/app/intro.md
+++
@@ -1,5 +0,0 @@
-# Hello, I'm Jack
-
-## Technology Architecture Consultant
-
-I'm a quick-learning and adaptable generalist with experience of solution architecture, devops and infrastructure engineering, and software development.
--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,30 +1,29 @@
 import Image from 'components/Image'
-import { Center, Flex, Space, Text } from '@mantine/core'
-import { join } from 'path'
-import { fixLocalPath } from 'utils/filepath'
-import { readLocalFile } from 'utils/markdown'
-import Markdown, { Components, defaultComponents } from 'components/Markdown'
+import { Center, Flex, Space, Title, Text } from '@mantine/core'
 import SocialLinks from 'components/SocialLinks'
 
 import WorkProfilePhoto from './work-profile-photo.jpg'
 
 export default async function Page() {
-  const intro = (await readLocalFile(fixLocalPath(join(__dirname, 'intro.md')))).content
-  const components: Components = {
-    ...defaultComponents,
-    p: (props) => <Text ta='center' >{props.children}</Text>,
-  }
-
   return (
     <Center>
       <Flex direction='column' w={600} >
         <Space/>
         <Center>
-          <Image src={WorkProfilePhoto} alt="Work profile / professional style image of Jack" w={400} radius='50%' />
+          <Image visibleFrom='xs' src={WorkProfilePhoto} alt="Work profile / professional style image of Jack" w={400} radius='50%' />
+          <Image hiddenFrom='xs' src={WorkProfilePhoto} alt="Work profile / professional style image of Jack" w={300} radius='50%' />
         </Center>
         <Space/>
         <Flex direction='column' align='center' justify='center' gap='xs' >
-          <Markdown components={components} >{intro}</Markdown>
+          <Title order={1} ta='center' >
+            Hello, I'm Jack
+          </Title>
+          <Title order={2} ta='center' >
+            Technology Architecture Consultant
+          </Title>
+          <Text ta='center' >
+            I'm a quick-learning and adaptable generalist with experience of solution architecture, devops and infrastructure engineering, and software development.
+          </Text>
           <Space h={5} />
           <SocialLinks/>
         </Flex>

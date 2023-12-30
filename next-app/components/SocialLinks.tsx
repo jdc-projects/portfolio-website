891905489944787faa04c2b4035115a69ec800bf
Jack Chapman <jack.chapman@sky.com>
2023-12-30 19:13:00 +0000

    Add email to social links.

--- next-app/components/SocialLinks.tsx
+++ next-app/components/SocialLinks.tsx
@@ -1,5 +1,5 @@
 import { Flex } from "@mantine/core";
-import { IconBrandGithub, IconBrandGithubFilled, IconBrandLinkedin } from "@tabler/icons-react";
+import { IconBrandGithub, IconBrandGithubFilled, IconBrandLinkedin, IconMail } from "@tabler/icons-react";
 import Anchor from "components/Anchor";
 
 export default function SocialLinks() {
@@ -8,6 +8,7 @@ export default function SocialLinks() {
       <Anchor href='https://github.com/jd-chapman' c='currentColor' ><IconBrandGithub size={48} stroke={1.5} /></Anchor>
       <Anchor href='https://github.com/jdc-projects' c='currentColor' ><IconBrandGithubFilled size={48} stroke={1.5} /></Anchor>
       <Anchor href='https://www.linkedin.com/in/jack-chapman-' c='currentColor' ><IconBrandLinkedin size={48} stroke={1.5} /></Anchor>
+      <Anchor href='mailto: jack.chapman@sky.com' c='currentColor' ><IconMail size={48} stroke={1.5} /></Anchor>
     </Flex>
   )
 }

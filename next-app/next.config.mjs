b72f536a12f68a6eb6f4ac938e082abf4c23123a
Jack Chapman <jack.chapman@sky.com>
2024-02-08 23:04:11 +0000

    Writeup setup for GCP backup option.

--- next-app/next.config.mjs
+++ next-app/next.config.mjs
@@ -32,6 +32,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'placehold.co',
       },
+      {
+        protocol: 'https',
+        hostname: '*.iconduck.com',
+      },
     ],
   },
   experimental: {

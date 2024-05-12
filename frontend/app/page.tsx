abb9aff6c0074deb7b009934024bb700ea5cb963
Jack Chapman <jack.chapman@sky.com>
2024-05-12 14:51:24 +0100

    Revert "TEMP debugging."
    This reverts commit bfda7dc66a414b099dae815c7aa52ecb568a247c.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -16,7 +16,7 @@ export default async function Page() {
         <Space/>
         <Flex direction='column' align='center' justify='center' gap='xs' >
           <Title order={1} ta='center' >
-            {process.env.NEXT_PUBLIC_BACKEND_URL}
+            {"Hello, I'm Jack"}
           </Title>
           <Title order={2} ta='center' >
             {"Technology Architecture Consultant"}

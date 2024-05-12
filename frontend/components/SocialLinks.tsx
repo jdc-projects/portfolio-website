6aa6a58cf7eeeeb40b6dfefe7afa044ebae5e47d
Jack Chapman <jack.chapman@sky.com>
2024-05-12 17:03:26 +0100

    Remove personal github account link.

--- frontend/components/SocialLinks.tsx
+++ frontend/components/SocialLinks.tsx
@@ -1,12 +1,11 @@
 import { Flex } from "@mantine/core";
-import { IconBrandGithub, IconBrandGithubFilled, IconBrandLinkedin, IconMail } from "@tabler/icons-react";
+import { IconBrandGithub, IconBrandLinkedin, IconMail } from "@tabler/icons-react";
 import Anchor from "components/Anchor";
 
 export default function SocialLinks() {
   return (
     <Flex direction='row' gap='xl' >
-      <Anchor href='https://github.com/jd-chapman' c='currentColor' ><IconBrandGithub size={48} stroke={1.5} /></Anchor>
-      <Anchor href='https://github.com/jdc-projects' c='currentColor' ><IconBrandGithubFilled size={48} stroke={1.5} /></Anchor>
+      <Anchor href='https://github.com/jdc-projects' c='currentColor' ><IconBrandGithub size={48} stroke={1.5} /></Anchor>
       <Anchor href='https://www.linkedin.com/in/jack-chapman-' c='currentColor' ><IconBrandLinkedin size={48} stroke={1.5} /></Anchor>
       <Anchor href='mailto: jack.chapman@sky.com' c='currentColor' ><IconMail size={48} stroke={1.5} /></Anchor>
     </Flex>

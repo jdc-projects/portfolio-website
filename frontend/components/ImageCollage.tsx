ecf4763955bb28a5bac25e14d2cb15e33264933c
Jack Chapman <jack.chapman@sky.com>
2024-05-20 19:01:06 +0100

    Add images for skills section in project writeups.

---
+++ frontend/components/ImageCollage.tsx
@@ -0,0 +1,22 @@
+import Image, { ImageProps } from 'components/Image'
+import { Grid, GridCol } from '@mantine/core'
+
+type ImageCollageProps = {
+  imagesProps: Array<ImageProps>
+}
+
+export default function ImageCollage(props: ImageCollageProps) {
+  const Images = props.imagesProps.map( imageProps => {
+    return (
+      <GridCol span={4} key={imageProps.alt} >
+        <Image {...imageProps} radius={0} />
+      </GridCol>
+    )
+  })
+
+  return (
+    <Grid my={20} justify='center' align='center' gutter={{ base: 30 }} >
+      {Images}
+    </Grid>
+  )
+}

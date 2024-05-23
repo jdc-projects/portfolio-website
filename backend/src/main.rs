3c757f84ea83ee5e9dda410e84c4d189bba50056
Jack Chapman <jack.chapman@sky.com>
2024-05-23 17:54:58 +0100

    Simple rust backend set up.

---
+++ backend/src/main.rs
@@ -0,0 +1,14 @@
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run our app with hyper
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}

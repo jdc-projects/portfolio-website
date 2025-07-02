600a1ea7b04b40817a061b52a89483526509127f
Jack Chapman <jack.chapman@sky.com>
2025-07-02 17:19:35 +0100

    Fix for drawio SVGs.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -62,6 +62,34 @@ const nextConfig = {
       '@mantine/core',
     ],
   },
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg'),
+    )
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports using url-loader
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: [
+          'url-loader',
+        ],
+      },
+    )
+
+    return config
+  },
 }
 
 export default withBundleAnalyser(withMDX(nextConfig))
